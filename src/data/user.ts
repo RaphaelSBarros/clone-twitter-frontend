953d37dfd007fd18300d19dc6422f126eef44048
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-10-11 10:04:41 -0300

    Layout do lado esquerdo

---
+++ src/data/user.ts
@@ -0,0 +1,12 @@
+import { User } from '@/types/user'
+
+export const user: User = {
+  slug: 'usuario',
+  name: 'Usuário Fake',
+  avatar: 'https://ui-avatars.com/api/?background=random&name=Usuario+Fake',
+  cover:
+    'https://img.freepik.com/fotos-gratis/conceito-de-viagem-completo-com-pontos-de-referencia_23-2149153258.jpg?t=st=1728649503~exp=1728653103~hmac=489d23bc8cb182e0225902026b8e8b7b2b58badf2a7ed2d916947158ae492d9c&w=826',
+  bio: 'Desenvolvedor sênior há mais de 14 anos',
+  link: 'https://google.com.br',
+  postCount: 127,
+}
